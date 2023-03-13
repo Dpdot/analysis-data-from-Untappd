e43acbfeef776e4c5ad6ec137ecf68be3fa39b89
Dpdot <81508363+Dpdot@users.noreply.github.com>
2023-03-13 18:57:16 +0100

    Update magisterka_untappd.R

--- magisterka_untappd.R
+++ magisterka_untappd.R
@@ -2,7 +2,7 @@ library(httr)
 library(rjson)
 
 untappdAPI <- function(adres) {
-  adres_url <- paste(adres,"&client_id=BEE5D99036C1A685A993F03F2112D91372339C48&client_secret=923B3926CB8482D5AF8CCC7661929E129DA4FAC0", sep = "")
+  adres_url <- paste(adres,"&client_id=", sep = "")
   
   wez <- GET(adres_url)
   
